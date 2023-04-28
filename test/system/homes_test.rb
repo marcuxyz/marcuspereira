0d2b112b08656ab899f32110b2c6fb7f1ff9eafd
Marcus Pereira <mpereirassa@gmail.com>
2023-04-28 12:50:00 -0300

    ✨ feat(shared/_me.html.erb): remove imagem do perfil 🧪 test(system/homes_test.rb): adiciona testes para redirecionamento de perfis sociais e curso
    A imagem do perfil foi removida para melhorar a privacidade do usuário. Os testes foram adicionados para garantir que os links para os perfis sociais e o curso estejam funcionando corretamente.

---
+++ test/system/homes_test.rb
@@ -0,0 +1,55 @@
+require 'application_system_test_case'
+
+class HomesTest < ApplicationSystemTestCase
+  driven_by :selenium, using: :headless_chrome
+
+  test 'should redirect to twitter profile' do
+    visit root_path
+
+    find('i.fa-twitter').click
+
+    page.driver.browser.switch_to.window page.driver.browser.window_handles.last do
+      assert current_url == 'https://twitter.com/marcuxyz'
+    end
+  end
+
+  test 'should redirect to linkedin profile' do
+    visit root_path
+
+    find('i.fa-linkedin').click
+
+    page.driver.browser.switch_to.window page.driver.browser.window_handles.last do
+      assert current_url == 'https://www.linkedin.com/in/marcuxyz/'
+    end
+  end
+
+  test 'should redirect to github profile' do
+    visit root_path
+
+    find('i.fa-github').click
+
+    page.driver.browser.switch_to.window page.driver.browser.window_handles.last do
+      assert current_url == 'https://github.com/marcuxyz'
+    end
+  end
+
+  test 'should redirect to youtube profile' do
+    visit root_path
+
+    find('i.fa-youtube').click
+
+    page.driver.browser.switch_to.window page.driver.browser.window_handles.last do
+      assert current_url == 'https://www.youtube.com/@MarcusPereira2022'
+    end
+  end
+
+  test 'should redirect to Flask Masterclass course' do
+    visit root_path
+
+    click_on 'Flask Masterclass'
+
+    page.driver.browser.switch_to.window page.driver.browser.window_handles.last do
+      assert current_url == 'https://www.youtube.com/playlist?list=PLxIrGEv-x-afU0SA3TIaLn4sAqtLqse06'
+    end
+  end
+end
