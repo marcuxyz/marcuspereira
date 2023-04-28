d005e94432ff0936c06748e6455a1ba4df496d53
Marcus Pereira <mpereirassa@gmail.com>
2023-04-28 11:37:19 -0300

    ✨ feat(home_controller.rb): adiciona variável de instância @posts no método index ✨ feat(home/index.html.erb): itera sobre a variável de instância @posts para exibir os posts no mural ✨ feat(application.rb): configura o fuso horário padrão para America/Sao_Paulo e o idioma padrão para pt-BR ✨ feat(posts.pt-BR.yml): adiciona traduções para o modelo Post ✨ feat(pt-BR.yml): adiciona traduções para o idioma pt-BR ✨ feat(seeds.rb): cria 3 registros de post usando FactoryBot ✨ feat(home_test.rb): adiciona testes para verificar se os posts são exibidos no mural O método index do HomeController agora define uma variável de instância @posts que contém todos os registros de post. O arquivo home/index.html.erb agora itera sobre a variável

--- test/integration/home_test.rb
+++ test/integration/home_test.rb
@@ -4,15 +4,28 @@ class HomeTest < ActionDispatch::IntegrationTest
   test 'should contains text in index' do
     get root_path
 
-    assert response.body.include? 'MARCUS'
-    assert response.body.include? 'PEREIRA'
-    assert response.body.include? 'O SENHOR é o meu pastor; nada me faltará. Salmos 23:1 NAA'
+    assert_includes response.body, 'MARCUS'
+    assert_includes response.body, 'PEREIRA'
+    assert_includes response.body, 'O SENHOR é o meu pastor; nada me faltará. Salmos 23:1 NAA'
   end
 
   test 'should contains mural' do
     get root_path
 
-    assert response.body.include? 'Mural'
-    assert response.body.include? 'Visualize informações, atualizações de eventos e cursos.'
+    assert_includes response.body, 'Mural'
+    assert_includes response.body, 'Visualize informações, atualizações de eventos e cursos.'
+  end
+
+  test 'should contains posts in mural' do
+    travel_to Time.zone.local(2023, 4, 28, 11, 20, 30)
+
+    create(:post, title: 'Ruby on Rails - Uma maneira diferente de aprender',
+                  content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.')
+
+    get root_path
+
+    assert_includes response.body, 'Ruby on Rails - Uma maneira diferente de aprender'
+    assert_includes response.body, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+    assert_includes response.body, '28 de abril, 2023.'
   end
 end
